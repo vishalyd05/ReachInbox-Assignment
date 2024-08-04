b3c74705be5232cc2d4235d339304cd54e6c167c
vishalyd05 <vydhoni57@gmail.com>
2024-08-05 02:19:54 +0530

    Bar setup

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,16 @@
+function AppBar() {
+  return (
+    <div className="border-[#25262B] bg-black border-b-2 fixed text-white h-16 w-screen flex items-center justify-center">
+      <div className="">
+        {/* <img
+          src="https://s3-alpha-sig.figma.com/img/37f3/2626/1680d9613629542a2474f79f92f7c022?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LdSHw32JdrAUCkZRjDegWHYZhYUXcG6e2p8mTF1znw5zXWZW-TqvZCClVuWpv8TPN4~WkC6RsFGw-~YOYAQG9N9jQlq6AmwKkc0UQlFG~lvi3VoW5BWABVAHcvGsd4vWHlawlpAlGgbSmpQ2btm7ganm5Uh48muweUExKndygFpePj2IFO~FNwibWHx~UsFP4iClPtQPDgYJ67pWrvxCOtl56IeC~YH94Nw6Bez-t23owH07qAhjjL2H9oKb0n3TGvJAXcG1kg2QvDHDgeOG~4NWqwMNvp7SjMejmcCOKNIFKUHqCXFW52LMzPIhfHLKVHZXuLK0vOKzegkuJSbexw__"
+          alt="logo"
+          className=" h-10"
+        ></img> */}
+        <h1 className="text-2xl font-bold">REACHINBOX</h1>
+      </div>
+    </div>
+  );
+}
+
+export default AppBar;
